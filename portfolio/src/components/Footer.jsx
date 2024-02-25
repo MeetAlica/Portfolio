b091276f007abe6be78c3a6ce675ec621e9a649e
MeetAlica <feher.ali.96@gmail.com>
2024-02-25 17:30:22 +0100

    Created Footer component

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="max-w-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto">
+      <span className="primary-color">Aladár Fehér</span>
+      <p className="text-gray-600">
+        feher.ali.96@gmail.com <br /> #FullStackDev2024
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
