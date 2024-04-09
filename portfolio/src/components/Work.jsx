38f2560620b92a2afb52aef63446b758d7c7de13
MeetAlica <feher.ali.96@gmail.com>
2024-04-09 18:01:57 +0200

    Added todo app to work

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -12,6 +12,25 @@ const Work = () => {
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
+          <img src={proj3} alt="" className="max-h-full" />
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
+            <span className="text-2x1 font-bold text-white tracking-wider">
+              My portfolio website.
+            </span>
+            <div className="pt-8 text-center">
+              <a
+                href="https://github.com/MeetAlica/Portfolio/tree/main/portfolio"
+                target="_blank"
+              >
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  See on GitHub
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
           <img src={proj1} alt="" className="max-h-full" />
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
             <span className="text-2x1 font-bold text-white tracking-wider">
@@ -34,11 +53,10 @@ const Work = () => {
           <img src={proj2} alt="" className="max-h-full" />
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
             <span className="text-2x1 font-bold text-white tracking-wider">
-              A simple gallows game where you have to guess the password. A
-              weather API is also built in.
+              A very simple todo app.
             </span>
             <div className="pt-8 text-center">
-              <a href="https://hangman-game-8u1.pages.dev/" target="_blank">
+              <a href="https://todo-app-4f5.pages.dev/" target="_blank">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   Try it!
                 </button>
@@ -48,18 +66,16 @@ const Work = () => {
         </div>
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative">
-          <img src={proj3} alt="" className="max-h-full" />
+          <img src={proj2} alt="" className="max-h-full" />
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
             <span className="text-2x1 font-bold text-white tracking-wider">
-              My portfolio website.
+              A simple gallows game where you have to guess the password. A
+              weather API is also built in.
             </span>
             <div className="pt-8 text-center">
-              <a
-                href="https://github.com/MeetAlica/Portfolio/tree/main/portfolio"
-                target="_blank"
-              >
+              <a href="https://hangman-game-8u1.pages.dev/" target="_blank">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  See on GitHub
+                  Try it!
                 </button>
               </a>
             </div>
