f01c02c0da4b669ece3232ecc4868cc96db1122f
MeetAlica <feher.ali.96@gmail.com>
2024-07-03 18:49:01 +0200

    Hotfix

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
   const message = "Don't forget to:";
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[70vh] mx-auto py-8 bg-black">
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[auto] mx-auto py-8 bg-black">
       <div className="col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]">
         <img src={heroimage} alt="hero image" />
       </div>
 
       <div className="col-span-2 px-5 my-auto">
-        <h1 className="text-white text-4xl sm:text-5xl lg:text-8xl font-extrabold">
+        <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-extrabold">
           <span className="primary-color">{message}</span> <br />
           <TypeAnimation
             sequence={[
